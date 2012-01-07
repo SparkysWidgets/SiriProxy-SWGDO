ffd45247fcf880e2a2aa977c681cd7bde918056e
Sparky's Widgets <SparkysWidgets@gmail.com>
2012-01-07 14:31:10 -0800

    Minor change for further additions

--- lib/siriproxy-swgdo.rb
+++ lib/siriproxy-swgdo.rb
@@ -50,7 +50,7 @@ class SiriProxy::Plugin::SWGDO < SiriProxy::Plugin
       if(number = 01) #process their response
         say "opening door 1!"
 	sp.write "C1"
-      else
+      end
        if(number = 02) #process their response
         say "opening door 2!"
 	sp.write "C2"
